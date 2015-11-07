e7b9095f72d2c07420004b3b3ada92090fd81615
Mike Ormond <mormond@microsoft.com>
2015-11-07 15:40:10 +0000

    Updated date logic

--- appread/home/home.js
+++ appread/home/home.js
@@ -1,6 +1,6 @@
-(function(){
+(function () {
   'use strict';
-  
+
   var serviceApiEndpoint = 'https://customerdataservicedemo.azurewebsites.net/api/';
   var customerQuery = 'customer/';
   var orderQuery = 'order/'
@@ -8,113 +8,112 @@
   var spin8;
 
   // The Office initialize function must be run each time a new page is loaded
-  Office.initialize = function(reason){
-    jQuery(document).ready(function(){
+  Office.initialize = function (reason) {
+    jQuery(document).ready(function () {
       app.initialize();
-      
-        if ($.fn.Pivot) {
-          $('.ms-Pivot').Pivot();
-        }
+
+      if ($.fn.Pivot) {
+        $('.ms-Pivot').Pivot();
+      }
 
       spin8 = fabric.Spinner(jQuery("#spinner-8point")[0]);
       //spin8.start();
       displayItemDetails();
     });
   };
-  
+
   function customerLookupCallback(result) {
    
     //spin8.stop(); 
          
-   if (result.length > 0) {    
+    if (result.length > 0) {
       customer.lastName = result[0].LastName;
       customer.firstName = result[0].FirstName;
       customer.email = result[0].Email;
       customer.companyName = result[0].CompanyName;
       customer.customerId = result[0].CustomerId;
-      
+
       $('#company').text(customer.companyName);
       $('#lastName').text(customer.lastName);
       $('#firstName').text(customer.firstName);
       $('#number').text(customer.customerId);
-      
+
       $("#customer").removeClass("hidden");
-      
+
       getOrders(customer.customerId);
-   }
-   else
-   {
+    }
+    else {
       $("#uknownCustomer").removeClass("hidden");
-   }
+    }
   }
-    
+
   function getCustomer(email) {
-    
+
     $.ajax(
-        {
-            type: "GET",
-            dataType: "jsonp",
-            url: serviceApiEndpoint + customerQuery + email + "/true/",
-            contentType: "text/javascript",
-            success: customerLookupCallback
-        });
+      {
+        type: "GET",
+        dataType: "jsonp",
+        url: serviceApiEndpoint + customerQuery + email + "/true/",
+        contentType: "text/javascript",
+        success: customerLookupCallback
+      });
   }
-  
-  function makeCell(text)
-  {
+
+  function makeCell(text) {
     var cell = $('<div class="ms-Grid-col ms-u-sm3 block"></div>');
     var span = $('<span></span>').text(text);
     span.appendTo(cell);
-    
-    return cell;    
+
+    return cell;
   }
-  
+
   function orderLookupCallback(result) {
     var orders = result;
+    var lastOrder = new Date(1900, 1, 1);
     
-    var lastOrder = new Date(1900,1,1);
+    // for (var index = 0; index < orders.length; index++) {
+    //   
+    //   var orderDate = new Date(orders[index].PurchaseDate);     
+    //   if (orderDate > lastOrder) {
+    //     lastOrder = orderDate;
+    //   }
+    // }
     
-    for (var index = 0; index < orders.length; index++) {
-      
-      var orderDate = new Date(orders[index].PurchaseDate);
-      
+    orders.forEach(function (element) {
+
+      var orderDate = new Date(element.PurchaseDate);
       if (orderDate > lastOrder) {
         lastOrder = orderDate;
       }
-    }
-    
-    $("#lastOrder").text(lastOrder.getDate());
 
-    orders.forEach(function(element) {
+      var row = $('<div class="ms-Grid-row"></div>');
 
-    var row = $('<div class="ms-Grid-row"></div>');
-    
-    makeCell(element.CustomerId).appendTo(row);
-    makeCell(element.PurchaseDate).appendTo(row);
-    makeCell(element.InvoiceDate).appendTo(row);
-    makeCell(element.TotalAmount).appendTo(row);
-        
-    row.appendTo($('#orders'));
-    
+      makeCell(element.CustomerId).appendTo(row);
+      makeCell(element.PurchaseDate).appendTo(row);
+      makeCell(element.InvoiceDate).appendTo(row);
+      makeCell(element.TotalAmount).appendTo(row);
+
+      row.appendTo($('#orders'));
+
     }, this);
 
-    
+    $("#lastOrder").text(lastOrder.toDateString());
   }
-  
+
   function getOrders(customerId) {
     $.ajax(
-        {
-            type: "GET",
-            dataType: "jsonp",
-            url: serviceApiEndpoint + orderQuery + customerId + "/",
-            contentType: "text/javascript",
-            success: orderLookupCallback
-        });
+      {
+        type: "GET",
+        dataType: "jsonp",
+        url: serviceApiEndpoint + orderQuery + customerId + "/",
+        contentType: "text/javascript",
+        success: orderLookupCallback
+      });
   }
 
 
   // Displays the "Subject" and "From" fields, based on the current mail item
-  function displayItemDetails(){
+  function displayItemDetails() {
     var item = Office.cast.item.toItemRead(Office.context.mailbox.item);
 
     //jQuery('#subject').text(item.subject);
@@ -127,7 +126,7 @@
     }
 
     if (from) {
-      
+
       getCustomer(from.emailAddress);
       
       // jQuery('#from').text(from.displayName);
